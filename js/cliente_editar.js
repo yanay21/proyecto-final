cb84ebefa90e117b65c6f9e6020b0ea2c849dd3e
yanay <yanayschvartz@gmail.com>
2023-12-06 19:05:51 +0200

    crud

---
+++ js/cliente_editar.js
@@ -0,0 +1,64 @@
+console.log(location.search) // lee los argumentos pasados a este formulario
+var id=location.search.substr(4)
+console.log(id)
+const { createApp } = Vue
+    createApp({
+        data() {
+            return {
+                id:0,
+                nombre:"",
+                genero:"",
+                plan:"",
+                edad:0,
+                hijos:0,
+                
+                url:'http://yanay23.pythonanywhere.com/clientes'+id,
+            }
+        },
+        methods: {
+            fetchData(url) {
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                        this.id=data.id
+                        this.nombre = data.nombre;
+                        this.genero=data.genero
+                        this.edad=data.edad
+                        this.hijos=data.hijos
+                        this.plan=data.plan
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        this.error=true
+                    })
+            },
+            modificar() {
+                let cliente = {
+                    nombre:this.nombre,
+                    edad: this.edad,
+                    genero: this.genero,
+                    hijos:this.hijos,
+                    plan:this.plan
+                }
+                var options = {
+                    body: JSON.stringify(cliente),
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json' },
+                    redirect: 'follow'
+                }
+                fetch(this.url, options)
+                    .then(function () {
+                        alert("Registro modificado")
+                        window.location.href = "./clientes.html";
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        alert("Error al Modificar")
+                    })
+            }
+        },
+        created() {
+            this.fetchData(this.url)
+        },
+    }).mount('#app')
---
+++ js/clientes.js
@@ -0,0 +1,72 @@
+const { createApp } = Vue
+    createApp({
+        data() {
+            return {
+                clientes:[],
+                url:"http://yanay23.pythonanywhere.com/clientes",
+               
+                error:false,
+                cargando:true,
+                /*atributos para el guardar los valores del formulario */
+                id:0,
+                nombre:"",
+                genero:"",
+                plan:"",
+                edad:0,
+                hijos:0,
+            }
+        },
+        methods: {
+            fetchData(url) {
+
+                fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.clientes = data;
+                    this.cargando=false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true
+                })
+            },
+            eliminar(productocliente) {
+                const url = this.url+'/' + producto;
+                var options = {
+                    method: 'DELETE',
+                }
+                fetch(url, options)
+                    .then(res => res.text()) // or res.json()
+                    .then(res => {
+                        location.reload();
+                    })
+            },
+            grabar(){
+                let cliente = {
+                    nombre:this.nombre,
+                    edad: this.edad,
+                    genero: this.genero,
+                    hijos:this.hijos,
+                    plan:this.plan
+                }
+                var options = {
+                    body:JSON.stringify(cliente),
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    redirect: 'follow'
+                }
+                fetch(this.url, options)
+                    .then(function () {
+                        alert("Registro grabado")
+                        window.location.href = "./clientes.html";
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        alert("Error al Grabarr")
+                    })
+            }
+        },
+        created() {
+            this.fetchData(this.url)
+        },
+    }).mount('#app')
